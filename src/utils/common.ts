71758bfe82e71e5b29bf7495a73ea5d7b2cc68c7
吴泽城 <wuzechengnanuo@dingtalk.com>
2022-06-30 17:07:51 +0800

    公共方法修改

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,7 +3,11 @@ import store from '@/store'
 import { cloneDeep } from 'lodash';
 
 const common = {
-  // 深拷贝
+  /**
+   * 深拷贝
+   * @param obj 拷贝的目标对象
+   * @returns 
+   */
   copy: (obj:any) => {
     return cloneDeep(obj);
   },
@@ -31,7 +35,7 @@ const common = {
     return Object.prototype.toString.call(str).slice(8, -1) === 'String';
   },
   // 是否为空， 包括 空数组，空对象， null、空字符串、undefined、NaN
-  isEmpty: (val:any, type:any = false) => {
+  isEmpty: (val:any, type = false) => {
     if (['', null, 'undefined', undefined].includes(type && common.isString(val) ? val.trim() : val)) return true;
     if (common.isJson(val)) return Object.keys(val).length === 0;
     if (common.isArray(val)) return val.length === 0;
@@ -65,73 +69,111 @@ const common = {
   isSet: (val:any) => {
     return Object.prototype.toString.call(val).slice(8, -1) === 'Set'
   },
-  // 数组去重
-  arrRemoveRepeat: (arr:any) => {
+  /**
+   * 数组去重
+   * @param arr 转换目标数组
+   * @returns 
+   */
+  arrRemoveRepeat: (arr:Array<any>) => {
     return Array.from(new Set(arr));
   },
-  // 移除对象中所有为字符串的值 2 端空格
-  // exceptionKey 不处理的键名
-  trim: (obj:any, exceptionKey:any, type = false) => {
-    const exception = common.isString(exceptionKey) ? [exceptionKey] : exceptionKey || [];
-    if (common.isEmpty(obj, true)) return '';
-    let newObj = type ? obj : common.copy(obj);
-    if (common.isString(newObj)) return obj.trim();
-    if (common.isObject(newObj)) {
-      if (common.isArray(newObj)) {
-        newObj.forEach((item:any, index:any) => {
-          if (common.isObject(item)) {
-            common.trim(item, exception, true);
-          } else if (common.isString(item)) {
-            newObj[index] = item.trim();
+    /**
+   * 多维数组扁平化
+   * @param arr 转换目标数组
+   * @returns 
+   */
+  flat: (arr:Array<any>) => {
+    return arr.flat(Infinity);
+  },
+  /**
+   * 移除对象中所有为字符串的值 2 端空格， 不影响原数据
+   * @param target 处理的模板对象
+   * @param ruleOut 不处理的键名, 包括所在的所有子级, 可以指定对象数据链，如果是数组可以: a.b.c[*].d.g[*].key, * 号也使用数字代替: a.b.c[3].d.g[2].key
+   * @returns 
+   */
+  trim (target:string | {[key:string]:any} | Array<any>, ruleOut?:string | Array<string>) {
+    const outKey = !common.isEmpty(ruleOut) ? (typeof ruleOut == 'string' ? [ruleOut] : Array.isArray(ruleOut) ? ruleOut : []) : [];
+    const hand = (obj:string | {[key:string]:any} | Array<any>, stackPointer?: string, stackPointerLike?:string) => {
+      if (typeof obj === 'string') return obj.trim();
+      if (!common.isObject(obj)) return obj;
+      let backVal;
+      if (Array.isArray(obj)) {
+        backVal = [];
+        obj.forEach((item:any, index) => {
+          const currentKey = `${common.isEmpty(stackPointer)?'':stackPointer}[${index}]`;
+          const currentLikeKey = `${common.isEmpty(stackPointerLike)?'':stackPointerLike}[*]`;
+          if (!outKey.includes(currentKey) && !outKey.includes(currentLikeKey)) {
+            if (common.isObject(item)) {
+              backVal.push(hand(item, currentKey, currentLikeKey))
+            } else {
+              typeof item == 'string' ? backVal.push(item.trim()) : backVal.push(item);
+            }
+          } else {
+            backVal.push(item);
           }
         })
-      } else if (common.isJson(newObj)) {
-        Object.keys(newObj).forEach(key => {
-          if (common.isObject(newObj[key])) {
-            common.trim(newObj[key], exception, true);
-          } else if (common.isString(newObj[key]) && !exception.includes(key)) {
-            newObj[key] = newObj[key].trim();
+      } else if (common.isJson(obj)) {
+        backVal = {};
+        Object.keys(obj).forEach((key:any) => {
+          const currentKey = `${common.isEmpty(stackPointer) ? '' : `${stackPointer}.`}${key}`;
+          const currentLikeKey = `${common.isEmpty(stackPointerLike) ? '' : `${stackPointerLike}.`}${key}`;
+          if (!outKey.includes(key) && !outKey.includes(currentKey) && !outKey.includes(currentLikeKey)) {
+            if ( common.isObject(obj[key])) {
+              backVal[key] = hand(obj[key], currentKey, currentLikeKey);
+            } else {
+              backVal[key] = typeof obj[key] == 'string' ? obj[key].trim() : obj[key];
+            }
+          } else {
+            backVal[key] = obj[key];
           }
         })
       }
+      return backVal;
     }
-    return newObj;
+    return hand(common.copy(target));
   },
-  /* 获取全部url参数,并转换成json对象 */
-  getUrlParams (config:any = {}) {
+  /**
+   * 获取全部url参数,并转换成json对象
+   * @param config 目标对象
+   * @returns 
+   */
+  getUrlParams (config:{url?:string, keys?: Array<string> | string} = {}) {
     const newUrl = decodeURIComponent(config.url || window.location.href);
     if (newUrl.indexOf('?') === -1) return {};
     const urlOption = newUrl.substring(newUrl.indexOf('?') + 1);
     const urlList = urlOption.split('&');
-    let urlJson:any = {};
+    let urlJson:{[key:string|number]:string} = {};
     urlList.forEach(item => {
       const pos = item.indexOf('=');
       urlJson[item.substring(0, pos)] = item.substring(pos + 1);
     })
     if (config.keys && typeof config.keys === 'string') {
       return urlJson[config.keys];
-    } else if (config.keys && common.isArray(config.keys)) {
-      return config.keys.map((key:any) => {
+    } else if (config.keys && Array.isArray(config.keys)) {
+      return config.keys.map((key:string | number) => {
         return { [key]: urlJson[key] };
       })
     }
     return urlJson;
   },
-  // 复制到粘贴板上
+  /**
+   * 复制到粘贴板上
+   * @param content 需要复制的字符串或JSON
+   * @returns 
+   */
   copyToClip (content:any) {
-    let copyTxt = content;
-    if (common.isObject(copyTxt)) {
-      copyTxt = JSON.stringify(copyTxt);
-    }
+    const copyTxt = common.isObject(content) ? JSON.stringify(content) : content;
     return new Promise(resolve => {
       // execCommand 方法有可能弃用，，
       if (document.execCommand) {
-        let staging = document.createElement('input');
-        // const dome = document.querySelector('body')
-        staging.setAttribute('value', copyTxt);
+        let staging = document.createElement('textarea');
+        staging.innerHTML = copyTxt;
         document.body.appendChild(staging);
         staging.style.position = 'absolute';
-        staging.style.top = '-500px';
+        staging.style.maxHeight = '30vh';
+        staging.style.maxWidth = '30vw';
+        staging.style.top = '-1000vh';
+        staging.style.left = '1000vw';
         staging.select();
         document.execCommand('copy');
         document.body.removeChild(staging);
@@ -142,41 +184,62 @@ const common = {
       }
     })
   },
-  // base64 转为文件格式
-  base64ToFile (base64Data:any, fileName = '') {
-    const arr = base64Data.split(',');
-    const fileType = arr[0].match(/:(.*?);/)[1];
-    let bstr = atob(arr[1]);
+  /**
+   * base64 转为文件格式
+   * @param base64Data 需要转换的 base64
+   * @param fileName 文件名
+   * @param fileType 文件类型
+   * @returns 
+   */
+  base64ToFile (base64Data:string, fileName:string, fileType?: string) {
+    const arr:Array<string> = base64Data.split(',');
+    const match = arr[0].match(/:(.*?);/);
+    const newFileType = arr[0] && match && match.length > 1 ? match[1] : fileType;
+    let bstr = atob(arr.length > 1 ? arr[1] : base64Data);
     let leng = bstr.length;
     let UintArr = new Uint8Array(leng);
     while (leng--) {
       UintArr[leng] = bstr.charCodeAt(leng);
     }
-    const newFile = new File([UintArr], fileName, {
-      type: fileType
-    })
-    return newFile;
+    return !!newFileType ? new File([UintArr], fileName, {
+      type: newFileType
+    }) : new File([UintArr], fileName);
   },
-  // 获取cookie, 支持数组和json
-  getCookie (keys:any) {
+  /**
+   * 获取cookie, 支持数组和json
+   * @param keys 需要获取 cookie 的key
+   * @returns 
+   */
+  getCookie (keys:string | Array<string> | {[key:string]: string}) {
     if (common.isObject(keys)) {
-      let newVal:any = {};
+      let newVal:string | {[key:string]: string} = {};
       if (common.isJson(keys)) {
-        Object.keys(keys).forEach((key:any) => {
+        Object.keys(keys).forEach((key:string) => {
           newVal[key] = Cookies.get(keys[key]) || '';
         })
-      }
-      if (common.isArray(keys)) {
-        keys.forEach((key:any) => {
+      } else if (Array.isArray(keys)) {
+        keys.forEach((key:string) => {
           newVal[key] = Cookies.get(key) || '';
         })
       }
       return newVal;
     }
-    return common.isString(keys) ? (Cookies.get(keys) || '') : '';
+    return typeof keys === 'string' ? (Cookies.get(keys) || '') : '';
   },
-  // 插入cookie，支持数组和json
-  setCookie (key:any = null, val:any = null, expires = {}) {
+  /**
+   * 插入cookie，支持数组和json
+   * @param key 插入cookie 的key，此处可以是数组，json
+   * @param val cookie 的值(当 key 为数据或对象时，此参数会被忽略)
+   * @param expires cookie 的过期时间和以前设置(当 key 为数据或对象时，此参数会被忽略)
+   * @returns 
+   */
+  setCookie (
+    key:string | Array<{
+      key: string, value: string, expires?: {[key:string]: any}
+    }> | {
+      key: string, value: string, expires?: {[key:string]: any}
+    }, val?:string, expires?: {[key:string]: any}
+  ) {
     if (common.isEmpty(key)) return;
     if (common.isJson(key)) {
       Object.keys(key).forEach(item => {
@@ -184,59 +247,109 @@ const common = {
           Cookies.set(key[item].key, key[item].value, key[item].expires || {});
         }
       })
-    } else if (common.isArray(key)) {
-      key.forEach((item:any) => {
+    } else if (Array.isArray(key)) {
+      key.forEach((item) => {
         if(!common.isEmpty(item.key) && !common.isEmpty(item.value)) {
           Cookies.set(item.key, item.value, item.expires || {});
         }
       })
-    } else if(typeof key === 'string' && !common.isEmpty(key) && !common.isEmpty(val)){
-      Cookies.set(key, val, expires);
+    } else if(typeof key === 'string' && typeof val !== 'undefined'){
+      Cookies.set(key, val, expires||{});
     }
   },
-  // 移除,支持数组
-  delCookie (keys:any) {
-    if (typeof keys === 'string' && !common.isEmpty(keys)) {
+  /**
+   * 移除,支持数组
+   * @param keys 需要移除 cookie 的 key
+   * @returns 
+   */
+  delCookie (keys:string | Array<string>) {
+    if (common.isEmpty(keys)) return;
+    if (typeof keys === 'string') {
       Cookies.remove(keys);
-    } if (common.isArray(keys)) {
+    } else if (Array.isArray(keys)) {
       keys.forEach((item:any) => {
         common.delCookie(item);
       })
     }
   },
-  // 移除空值(不对原有数据修改)
-  // ruleOut 数据类型为数组，为空值时不移除的键名集合
-  removeEmpty (obj:any = {}, ruleOut = [] as any[], old:any = {}, oldKey:any = '', type = false) {
-    let newObj = !type ? common.copy(obj) : obj;
-    if (!common.isEmpty(newObj)) {
-      if (common.isArray(newObj)) {
-        old[oldKey] = newObj.filter((item:any) => {
-          return !common.isEmpty(item) && item !== 'web-null';
-        })
-        if (common.isEmpty(old[oldKey]) && !ruleOut.includes(oldKey)) {
-          delete old[oldKey]
-        } else {
-          old[oldKey].forEach((item:any, index:any) => {
-            common.isObject(item) && common.removeEmpty(item, ruleOut, old[oldKey], index, true);
+  /**
+   * 移除对象空值
+   * @param target 需要处理的目标对象
+   * @param ruleOut 不处理的键名, 包括所在的所有子级, 可以指定对象数据链，如果是数组可以: a.b.c[*].d.g[*].key, * 号也使用数字代替: a.b.c[3].d.g[2].key
+   * @param emptyAllClean 当子级为空时，递归清空父级, 默认为 false
+   * @returns 
+   */
+  removeEmpty: (target: Array<any>|{[key:string]: any}, ruleOut?:string | Array<string> | boolean, emptyAllClean?:boolean) => {
+    let outKey:Array<string> = [];
+    if (!common.isEmpty(ruleOut) && (typeof ruleOut == 'string' || Array.isArray(ruleOut))) {
+      outKey = typeof ruleOut == 'string' ? [ruleOut] : ruleOut;
+    } else if (typeof ruleOut == 'boolean') {
+      emptyAllClean = ruleOut;
+    }
+    const hand = (option:string | Array<any>|{[key:string]: any}, clean:boolean, stackPointer?:string, stackPointerLike?:string) => {
+      if (!common.isEmpty(option)) {
+        let newObj;
+        if (Array.isArray(option)) {
+          newObj = [];
+          option.forEach((item:any, index) => {
+            const currentKey = `${common.isEmpty(stackPointer)?'':stackPointer}[${index}]`;
+            const currentLikeKey = `${common.isEmpty(stackPointerLike)?'':stackPointerLike}[*]`;
+            if (!outKey.includes(currentKey) && !outKey.includes(currentLikeKey)) {
+              if (!common.isEmpty(item)) {
+                if (common.isObject(item)) {
+                  const newVal = hand(item, false, currentKey, currentLikeKey);
+                  if (!emptyAllClean && !clean) {
+                    common.isEmpty(newVal) && common.isJson(item) ? newObj.push(item) : newObj.push(newVal);
+                  } else {
+                    !common.isEmpty(newVal) && newObj.push(newVal);
+                  }
+                } else {
+                  newObj.push(item);
+                }
+              }
+            } else {
+              newObj.push(item);
+            }
+          })
+        } else if (common.isJson(option)) {
+          newObj = {};
+          Object.keys(option).forEach((key:any) => {
+            const currentKey = `${common.isEmpty(stackPointer) ? '' : `${stackPointer}.`}${key}`;
+            const currentLikeKey = `${common.isEmpty(stackPointerLike) ? '' : `${stackPointerLike}.`}${key}`;
+            if (!outKey.includes(key) && !outKey.includes(currentKey) && !outKey.includes(currentLikeKey)) {
+              if (!common.isEmpty(option[key]) && option[key] !== 'web-null') {
+                if (common.isObject(option[key])) {
+                  const newVal = hand(option[key], false, currentKey, currentLikeKey);
+                  if (!emptyAllClean && !clean) {
+                    newObj[key] = newVal;
+                  } else {
+                    !common.isEmpty(newVal) && (newObj[key] = newVal);
+                  }
+                } else {
+                  newObj[key] = option[key];
+                }
+              }
+            } else {
+              newObj[key] = option[key];
+            }
           })
         }
-      } else if (common.isJson(newObj)) {
-        Object.keys(newObj).forEach((key:any) => {
-          if ((common.isEmpty(newObj[key]) || newObj[key] === 'web-null') && !ruleOut.includes(key)) {
-            delete newObj[key];
-          } else if (common.isObject(newObj[key])) {
-            common.removeEmpty(newObj[key], ruleOut, obj, key, true);
-          }
-        })
+        return newObj;
       }
+      return option;
     }
-    return newObj;
+    return hand(common.copy(target), true);
   },
-  // 将对象转换为 get 请求方式类型
-  changeParams: (obj:any) => {
+  /**
+   * 将对象转换为 get 请求方式类型
+   * @param obj 需要转换的对象
+   * @param isDefault 是否带 ?
+   * @returns 
+   */
+  changeParams: (obj:{[key:string]: string}, isDefault?: boolean) => {
     if (common.isJson(obj)) {
       const keys = Object.keys(obj);
-      let params = '';
+      let params = isDefault ? '?' : '';
       keys.forEach(key => {
         params += params.includes('=')?`&${key}=${obj[key] }`:`${key}=${obj[key] }`;
       })
@@ -244,32 +357,39 @@ const common = {
     }
     return ''
   },
-  // 是有权限，支持 字符串、数组、json (当登录账号为 admin 时，全部返回 true)
-  getPower: (str:any) => {
+  /**
+   * 是有权限，支持 字符串、数组、json (当登录账号为 admin 时，全部返回 true)
+   * @param keys 需要获取的 key
+   * @returns 
+   */
+  getPower: (keys:string|Array<string>|{[key:string]:string}) => {
     const userInfo = store.getters['layout/userInfo'];
     const isPass = userInfo.loginName && ['admin', 'admin@lapa.com'].includes(userInfo.loginName);
     const permissionsIds = store.getters['layout/permissionsIds'];
-    if (common.isObject(str)) {
-      let newVal:any = {};
-      if (common.isJson(str)) {
-        Object.keys(str).forEach(key => {
-          newVal[key] = isPass || permissionsIds.includes(str[key]);
+    if (common.isEmpty(keys, true)) return isPass || false;
+    if (common.isObject(keys)) {
+      let newVal:{[key:string]:string} = {};
+      if (common.isJson(keys)) {
+        Object.keys(keys).forEach(key => {
+          newVal[key] = isPass || permissionsIds.includes(keys[key]);
         })
-      }
-      if (common.isArray(str)) {
-        str.forEach((key:any) => {
+      } else if (Array.isArray(keys)) {
+        keys.forEach(key => {
           newVal[key] = isPass || permissionsIds.includes(key);
         })
       }
       return newVal;
     }
-    return common.isString(str) ? (isPass || permissionsIds.includes(str)) : isPass;
+    return common.isString(keys) ? (isPass || permissionsIds.includes(keys)) : isPass;
   },
-  // 获取html节点设置的样式值
-  // element 目标节点或节点标识id,class等
-  // styleName 需要获取的样式名---对应 css 键名
-  // isNumber 是否返回数字，当值不支持 number 时，则原样返回
-  getElementStyle (element:any, styleName:any, isNumber = false) {
+  /**
+   * 获取html节点设置的样式值
+   * @param element 目标节点或节点标识id,class等
+   * @param styleName 需要获取的样式名---对应 css 键名
+   * @param isNumber 是否返回数字，当值不支持 number 时，则原样返回
+   * @returns 
+   */
+  getElementStyle (element:string | Element, styleName:string, isNumber?:boolean) {
     const newElement = typeof element === 'string' ? document.querySelector(element) : element;
     if (!newElement || common.isEmpty(styleName)) return isNumber ? 0 : null;
     // @ts-ignore
@@ -278,7 +398,7 @@ const common = {
     if (!isNumber || !style.includes('px')) return style;
     const styleArr = style.split(' ');
     let isPass = false;
-    let backVal:any = [];
+    let backVal:Array<any> = [];
     styleArr.forEach((item:any) => {
       if (!item.includes('px') || isNaN(parseInt(style))) {
         isPass = true;
@@ -287,7 +407,42 @@ const common = {
     })
     if (isPass) return style;
     return backVal.length === 1 ? backVal[0] : backVal;
+  },
+  /**
+   * 获取元素坐标(元素的左上角为基点)
+   * @param element 目标节点或节点标识id,class等标识
+   * @returns 
+   */
+  getElementOffset (element:string | Element) {
+    const newElement: any = typeof element === 'string' ? document.querySelector(element) : element;
+    let offset = { x: 0, y: 0 }
+    if (common.isEmpty(newElement)) return offset;
+    let current = newElement.offsetParent;
+    offset.x += newElement.offsetLeft;
+    offset.y += newElement.offsetTop;
+    while (current !== null) {
+      offset.x += current.offsetLeft || 0;
+      offset.y += current.offsetTop || 0;
+      current = current.offsetParent;
+    }
+    return offset;
+  },
+  /**
+   * 获取元素所在被滚动距离
+   * @param element 目标节点或节点标识id,class等标识
+   * @returns 
+   */
+  getElementScrollTop (element:string | Element) {
+    const newElement = typeof element === 'string' ? document.querySelector(element) : element;
+    if (!newElement) return 0;
+    let top = 0;
+    let current:any = newElement.parentNode;
+    while (current !== null) {
+      top += current.scrollTop || 0;
+      current = current.parentNode;
+    }
+    return top;
   }
 }
-// export default common;
+
 export default common;
