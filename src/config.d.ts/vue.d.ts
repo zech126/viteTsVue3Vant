16889dc711a109201ba5828e53c869cab35126b7
吴泽城 <wuzechengnanuo@dingtalk.com>
2022-08-01 17:20:02 +0800

    fix:部分属性提示

--- src/config.d.ts/vue.d.ts
+++ src/config.d.ts/vue.d.ts
@@ -1,23 +1,35 @@
 /// <reference types="vite/client" />
 import { AxiosInstance } from "axios";
 import type { commonClass } from "../utils/common";
-// import { Router } from 'vue-router'
+import dayjs from 'dayjs';
+import { ComponentInternalInstance, getCurrentInstance } from 'vue';
+import { Router, RouteLocationNormalizedLoaded } from "vue-router";
+import { Store } from "vuex";
+
+type PluginFunc<T = unknown> = (option: T, c: typeof dayjs.Dayjs, d: typeof dayjs) => void;
+
+interface RouteOptions extends RouteLocationNormalizedLoaded {
+  herf: string;
+}
 
 //全局配置（typescript使用）
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $http: AxiosInstance;
-    $common: commonClass;
-    $dayjs: any;
-    $toast: any;
-    $dialog: any;
-    $notify: any;
+    $http: Readonly<AxiosInstance>;
+    $common: Readonly<commonClass>;
+    $dayjs: {
+      extend:<T = unknown>(plugin: PluginFunc<T>, option?: T) => dayjs.Dayjs;
+      locale:(preset?: string | ILocale, object?: Partial<ILocale>, isLocal?: boolean) => string;
+      isDayjs: (d: any) => d is dayjs.Dayjs;
+      unix: (t: number) => dayjs.Dayjs;
+      (date?: dayjs.ConfigType, format?: dayjs.OptionType, locale?: string, strict?: boolean): dayjs.Dayjs;
+    };
+    $route: RouteOptions;
+    $router: Router;
+    $store: Store<any>;
     api: {[key:string]: any};
     $api: {[key:string]: any};
     $refs: any;
-    $attrs: any;
-    $emit: any;
-    $parent: any;
     [key:string]: any;
   }
 }
--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,15 +1,34 @@
 import type { commonClass } from "./common";
 import { AxiosInstance } from "axios";
-import { ComponentInternalInstance, getCurrentInstance } from 'vue'
+import dayjs from 'dayjs';
+import { ComponentInternalInstance, getCurrentInstance } from 'vue';
+import { Router, RouteLocationNormalizedLoaded } from "vue-router";
+import { Store } from "vuex";
+
+type PluginFunc<T = unknown> = (option: T, c: typeof dayjs.Dayjs, d: typeof dayjs) => void;
+
+interface RouteOptions extends RouteLocationNormalizedLoaded {
+  herf: string;
+}
+
 const getGlobal = () => {
-    const { appContext } = getCurrentInstance() as ComponentInternalInstance
-    return appContext.config.globalProperties as  {
-        $http: AxiosInstance;
-        $common: commonClass;
-        api: {[key:string]: any};
-        $api: {[key:string]: any};
-        $dayjs: any;
-        [key:string]:any;
-    }
+  const { appContext } = getCurrentInstance() as ComponentInternalInstance
+  return appContext.config.globalProperties as  {
+    $http: Readonly<AxiosInstance>;
+    $common: Readonly<commonClass>;
+    $dayjs: {
+      extend:<T = unknown>(plugin: PluginFunc<T>, option?: T) => dayjs.Dayjs;
+      locale:(preset?: string | ILocale, object?: Partial<ILocale>, isLocal?: boolean) => string;
+      isDayjs: (d: any) => d is dayjs.Dayjs;
+      unix: (t: number) => dayjs.Dayjs;
+      (date?: dayjs.ConfigType, format?: dayjs.OptionType, locale?: string, strict?: boolean): dayjs.Dayjs;
+    };
+    $route: RouteOptions;
+    $router: Router;
+    $store: Store<any>;
+    api: Readonly<{[key:string]: any}>;
+    $api: Readonly<{[key:string]: any}>;
+    [key:string]:any;
+  }
 }
 export default getGlobal;
